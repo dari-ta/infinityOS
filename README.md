4e8704e339aa544cdea81e9abf80f891315a652e
dari-ta (on infinityOS) <da.rita@gmx.de>
2015-02-27 20:48:39 +0100

    install step1 initial commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
+experimental!
+
 # infinityOS
 The next version of the infinityOS extensions for #!
 
 After the end of #!, I decided to rewrite my infinityOS extensions for nw and bundle it with a complete package and installer, for all who loved #!
+
+## install (experimental)
+
+### requirements
+* Arch Linux
+* Git (pacman -S git)
+
+### installation
+download this package with git.
+	git clone https://github.com/dari-ta/infinityOS
+go into the directory
+	cd infinityOS
+run the installer
+	./setup.sh --install
--- install/step1/setup.sh
+++ install/step1/setup.sh
@@ -1,1 +1,43 @@
- 
+## in step 1, we'll do post-install jobs, and install our choosen software
+
+echo "Step 1 - Core install"
+echo "======================"
+
+echo " >> install sudo"
+pacman -S sudo
+echo " we'll display you the sudoers file."
+echo " find the %wheel ALL=(ALL) ALL"
+echo " and remove the # hashtag before"
+echo " exit with Ctrl+x or :wq"
+read -p " ANY KEY to continue > " _answer
+EDITOR=nano visudo
+
+echo " >> install main services"
+pacman -S acpid ntp dbus avahi cronie
+systemctl enable cronie
+systemctl enable acpid
+systemctl enable ntpd
+systemctl enable avahi-daemon
+
+echo " >> install the X Window system"
+echo " you'll may be promted to choose your display driver"
+read -p " ANY KEY to continue > " _answer
+
+pacman -S xorg-server xorg-xinit xorg-utils xorg-server-utils
+
+read -P " are we on VirtualBox? (y/N) > " _answer
+if [[ $_answer = "y" ]]; then
+	pacman -S virtualbox-guest-utils
+fi
+
+echo " >> install window manager"
+pacman -S openbox
+mkdir -p ~/.config/openbox
+cp /etc/xdg/openbox/rc.xml ~/.config/openbox/
+cp /etc/xdg/openbox/menu.xml ~/.config/openbox/
+echo "exec openbox-session" >> ~/.xinitrc
+
+pacman -S obconf obmenu
+
+echo " >> install panel"
+pacman -S tint2
