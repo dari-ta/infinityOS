fb67db8b21146b127e083890749c547048fef612
dari-ta (on infinityOS) <da.rita@gmx.de>
2015-02-27 22:04:15 +0100

    Additions for more main apps

--- install/step1/apps.sh
+++ install/step1/apps.sh
@@ -1,11 +1,12 @@
 ## install the main apps
 ## * Terminator
+## * XTerm
 ## * Thunar (Archive Plugin, Volume Manager, Thumbnails
 ## * File Roller
 ## * Viewnior
 
 echo " >> install main apps"
-pacman --noconfirm -S terminator thunar thunar-archive-plugin file-roller thunar-volman raw-thumbnailer tumbler viewnior
+pacman --noconfirm -S terminator xterm thunar thunar-archive-plugin file-roller thunar-volman raw-thumbnailer tumbler viewnior
 
 ## Font
 echo " >> install fonts"
@@ -26,3 +27,34 @@ echo " >> install design packages"
 pacman --noconfirm -S lxappearance
 pacman --noconfirm -S hicolor-icon-theme elementary-icon-theme gnome-icon-theme
 pacman --noconfirm -S gtk-engines gtk-engine-murrine
+
+## Editor
+echo " >> install editor"
+pacman --noconfirm -S geany
+
+## Networking
+echo " >> install networking"
+pacman --noconfirm -S networkmanager network-manager-applet
+systemctl enable NetworkManager.service
+
+## Notifications, Keyring & Wallpaper
+echo " >> install notifications, keyring and wallpaper"
+pacman --noconfirm -S xfce4-notifyd gnome-keyring seahorse nitrogen
+
+## Sound
+echo " >> install sound system"
+pacman -S volwheel
+pacman -S pulseaudio paprefs pulseaudio-alsa alsa-utils gstreamer gstreamer0.10-base
+
+## System 
+echo " >> install system stuff"
+pacman --noconfirm -S lxtask clipit xf86-input-synaptics xfce4-power-manager
+pacman --noconfirm -S libconfig asciidoc
+pacman --noconfirm -S gksu polkit polkit-gnome
+
+## Node.js
+echo " >> install node.js"
+pacman --noconfirm -S nodejs
+npm -g install nw
+
+
