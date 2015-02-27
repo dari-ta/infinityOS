8ccabe9c1d49fcab538fa5b17760d1a3e45b0974
Daniel <daniel@dan-net.de>
2015-02-27 20:26:29 +0100

    First install script - not working yet

---
+++ setup.sh
@@ -0,0 +1,35 @@
+if ! [[ $1 = '--install' ]]; then
+	echo $1
+	echo 'Usage: setup.sh --install'
+	echo 'You will need root privileges'
+	exit 1
+fi
+
+echo 'Welcome to'
+echo '       __________       ___________         '
+echo '      / ________ \     / _________ \        '
+echo '     //          \\   //           \\       '
+echo '    //            \\ //             \\      '
+echo '    ||             |||              ||      '
+echo '    \\            // \\             //      '
+echo '     \\ _________//   \\ _________ //       '
+echo '      \__________/     \___________/        '
+echo '                                            '
+echo '                 infinity                   '
+echo ''
+echo ' Press `c` and ENTER to continue            '
+echo ' Press only ENTER to abort'
+echo ''
+
+read -p " > " _answer
+
+if ! [[ $_answer = 'c' ]]; then
+	exit 1
+fi
+
+# Copy all infinity installer files to /opt/infinity-install
+mkdir -p /opt/infinity-install
+cp -R install /opt/infinity-install
+
+# invoke the first install step
+/opt/infinity-install/step1/setup.sh
